4448669025f5831ef82ec45083f039030d0eaea0
Jakob Peters <jakobj.peters@gmail.com>
2022-04-25 19:48:22 -0700

    Add comments to 'config.jl'
    The comments show configuration options and other helpful information.

--- config.jl
+++ config.jl
@@ -1,7 +1,14 @@
 
+# GUI not implemented yet
 const display = terminal
+
+# ["mnist", "balanced", "digits", "bymerge", "byclass"]
+# 'letters' is broken
 dataset_name = "mnist"
+
+# must add to 100 (percent)
 splits = [80, 20]
+
 seed!(1)
 
 epochs = [
@@ -14,14 +21,25 @@ model = FFN(
     ModelHyperparameters((
         cost_func = squared_error,
         input_size = 784,
+
+        # not implemented yet
         precision = Float64
     )),
     LayerHyperparameters((
+        # [xavier, he]
         weight_init_funcs = [xavier, xavier],
+
         learn_rates = [0.01, 0.01],
+
+        # [z_score, demean, identity]
         norm_funcs = [z_score, identity],
+
+        # [sigmoid, tanh]
+        # relu does not work yet
         activ_funcs = [sigmoid, sigmoid],
+
         use_biases = [true, true],
-        sizes = [28, 10]
+
+        sizes = [28, length(mapping(dataset_name))]
     ))
 )
