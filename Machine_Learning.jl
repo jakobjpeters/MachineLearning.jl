7767db126aef947fe7dc27f7021ac75b943a5c8e
Jakob Peters <jakobj.peters@gmail.com>
2022-04-28 09:29:10 -0700

    Change 'config.jl' to 'config.TOML'
    Read 'config.TOML' in 'main'.
    Make conversions as needed.
    
    This is to remove 'config' as a global variable and prevents accessing it directly in later functions.

--- Machine_Learning.jl
+++ Machine_Learning.jl
@@ -7,9 +7,10 @@ using BenchmarkTools
 
 # External
 
-# Emnist
+# Files
 import GZip
 import ZipFile
+import TOML
 
 # Math
 import Distributions: Normal
@@ -26,8 +27,46 @@ include("core.jl")
 include("emnist.jl")
 include("print.jl")
 include("interface.jl")
-include("config.jl")
 
-display(dataset_name, dataset, epochs, model)
+function main()
+    config = TOML.parsefile("config.TOML")
+
+    # use metaprogramming?
+    seed = config["seed"]
+    display = config["display"]
+    data = config["data"]
+    epochs = config["epochs"]
+    model = config["model"]
+    layers = config["layers"]
+
+    string_to_func = string -> getfield(@__MODULE__, Symbol(string))
+    strings_to_funcs = strings -> map(string_to_func, strings)
+    float = Dict("Float16" => Float16, "Float32" => Float32, "Float64" => Float64)
+
+    ismissing(seed) ? seed!() : seed!(seed)
+    display = string_to_func(display)
+
+    preprocess = string_to_func(data["preprocessing_function"])
+    layer_sizes = vcat(layers["sizes"][begin:end - 1], length(mapping(data["name"])))
+
+    dataset = load_dataset(data["name"], preprocess, data["split_percentages"])
+    epochs = map(i -> Epoch(epochs["batch_size"], parse(Bool, epochs["shuffle_data"])), 1:epochs["n"])
+    model = Neural_Network(
+        string_to_func(model["cost_function"]),
+        784, # make dynamic
+        float[model["precision"]],
+        strings_to_funcs(layers["weight_initialization_functions"]),
+        strings_to_funcs(layers["normalization_functions"]),
+        strings_to_funcs(layers["activation_functions"]),
+        layers["learn_rates"],
+        layer_sizes,
+        layers["use_biases"]
+    )
+
+    display(data["name"], dataset, epochs, model)
+
+end
+
+main()
 
 end
