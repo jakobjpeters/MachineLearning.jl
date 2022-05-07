2dc90def38e985750ee239948b02a17f2b43655f
Jakob Peters <jakobj.peters@gmail.com>
2022-05-06 17:38:49 -0700

    Organize files

---
+++ src/core.jl
@@ -0,0 +1,95 @@
+
+# given input, calculate and cache 'Zs' and 'activations'
+function (dense::Dense)(inputs, activ_func, cache)
+    cache.Zs = dense.weights * inputs
+    if !isnothing(dense.biases)
+        cache.Zs .+= dense.biases
+    end
+
+    cache.activations = cache.Zs |> activ_func # |> layer.norm_func
+
+    # TODO: should 'return nothing'
+    return cache.activations
+end
+
+# call each layer with its correct parameters
+function (neural_net::Neural_Network)(inputs, h_params, caches)
+    for (layer, h_param, cache) in zip(neural_net.layers, h_params, caches)
+        # fix: allocating
+        inputs = layer(inputs, h_param.activ_func, cache)
+    end
+
+    return nothing
+end
+
+# given a model, calculate and cache the gradient for a batch of inputs
+function backpropagate!(layers, cost_func, h_params, caches, inputs, labels)
+    # TODO: improve
+    δl_δa = deriv(cost_func, caches[end].activations, labels)
+    prev_activations = pushfirst!(map(cache -> cache.activations, caches[begin:end - 1]), inputs)
+    
+    # iterate end to begin to calculate each layer's gradient
+    for (layer, cache, prev_activation, h_param) in zip(reverse(layers), reverse(caches), reverse(prev_activations), reverse(h_params))
+        δl_δz = δl_δa .* deriv(h_param.activ_func, cache.Zs)
+
+        # cache gradients
+        # gradients are averaged (divided by 'batch_size') in 'apply_gradient!' for efficiency
+        mul!(cache.δl_δw, δl_δz, transpose(prev_activation))
+        if layer.biases !== nothing
+            cache.δl_δb = dropdims(sum(δl_δz, dims = 2), dims = 2)
+        end
+
+        # if first layer, the following calculation is not needed
+        layer === first(layers) && break
+        δl_δa = transpose(layer.weights) * δl_δz
+    end
+
+    return nothing
+end
+
+# update weights and biases with cached gradient
+function apply_gradient!(layers, learn_rates, caches, batch_size)
+    # dividing by 'batch_size' turns the gradients from a sum to an average
+    scales = learn_rates / batch_size
+
+    # update each layer's weights and biases, then reset its cache
+    for (layer, cache, scale) in zip(layers, caches, scales)
+        # in-place 'a * X + Y', stored in Y
+        axpy!(scale, cache.δl_δw, layer.weights)
+        if layer.biases !== nothing
+            axpy!(scale, cache.δl_δb, layer.biases)
+        end
+    end
+
+    return nothing
+end
+
+# given a model and data, test the model and return its accuracy and loss
+function assess(cost_func, outputs, labels)
+    # TODO: parameterize decision criteria
+    criteria = pair -> argmax(first(pair)) == argmax(last(pair))
+
+    accuracy = count(criteria, zip(eachcol(outputs), eachcol(labels))) / size(outputs, 2)
+    cost = mean(cost_func(outputs, labels))
+
+    return accuracy, cost
+end
+
+# given a model and data, coordinate model training
+function (epoch::Epoch)(model, h_params, caches, inputs, labels)
+
+    if epoch.shuffle && epoch.batch_size < size(inputs, 1)
+        inputs, labels = shuffle_pair(inputs, labels)
+    end
+
+    # train model for each batch
+    for first in 1:epoch.batch_size:size(inputs, 2)
+        last = min(size(inputs, 2), first + epoch.batch_size - 1)
+    
+        model(view(inputs, :, first:last), h_params, caches)
+        backpropagate!(model.layers, epoch.cost_func, h_params, caches, view(inputs, :, first:last), view(labels, :, first:last))    
+        apply_gradient!(model.layers, map(h_params -> h_params.learn_rate, h_params), caches, epoch.batch_size)
+    end
+
+    return nothing
+end
---
+++ src/utilities.jl
@@ -0,0 +1,34 @@
+
+# given lists of inputs and labels, return a list of 'Data' split by percentages in 'splits'
+function split_data(inputs, labels, splits)
+    sum(splits) != 100 && error("Splits must add to 100 (percent)")
+
+    # generate indices to split the data by each percentage in 'splits'
+    starts = Vector{Int64}()
+    i = 0
+    for split in splits
+        append!(starts, div(i * size(inputs, 2), 100))
+        i += split
+    end
+    stops = append!(starts[begin + 1:end], size(inputs, 2))
+    starts .+= 1
+
+    return [Data(view(inputs, :, start:stop), view(labels, :, start:stop)) for (start, stop) in zip(starts, stops)]
+end
+
+# load selected dataset, preprocess dataset, and return list of dataset splits
+function load_dataset(name, preprocess, splits)
+    dataset = load_emnist(name)
+    prep_inputs = preprocess(dataset.inputs)
+    return split_data(prep_inputs, dataset.labels, splits)
+end
+
+# TODO: make this faster
+function shuffle_pair(inputs, labels)
+    # pair inputs and labels, then shuffle the pairs
+    data = shuffle!(collect(zip(eachcol(inputs), eachcol(labels))))
+    # unpack the pairs
+    data = getindex.(data, 1), getindex.(data, 2)
+    # transform to original structure
+    return reduce(hcat, data[1]), reduce(hcat, data[2])
+end
