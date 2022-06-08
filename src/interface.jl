f21adf6966e2cdc6460fcfab68ee18178b611e98
Jakob Peters <jakobj.peters@gmail.com>
2022-06-07 22:09:17 -0700

    Improve 'display' of 'config'

--- src/interface.jl
+++ src/interface.jl
@@ -1,18 +1,26 @@
 
-function terminal(config, key)
-    println()
-    println(key)
-    println(config)
+function indent(n)
+    for i in 1:n
+        print("\t")
+    end
 end
 
-function terminal(config::Dict)
-    for key in keys(config)
-        terminal(config[key], key)
+function terminal(config, n_indent = 0)
+    if config isa Dict
+        for key in keys(config)
+            indent(n_indent)
+            println(key)
+            terminal(config[key], n_indent + 1)
+        end
+    else
+        indent(n_indent)
+        println(config)
+        println()
     end
 end
 
 # prints model assessment for each data split
-function terminal(assessments)
+function terminal(assessments::AbstractArray{T}) where T <: NamedTuple
     println("\nEpoch: ", length(assessments) - 1)
     for (i, assessment) in enumerate(zip(last(assessments).accuracies, last(assessments).costs))
         @printf("\tSplit: %s\tAccuracy: %.4f\tCost: %.4f\n", i, first(assessment), last(assessment))
